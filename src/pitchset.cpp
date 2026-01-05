958fa222a87392e8783c1e5072c7422fa33d7f75
Peter Jung <peterjungx@gmail.com>
2026-01-05 17:53:12 +0100

    add missing import for linux gcc 2

--- src/pitchset.cpp
+++ src/pitchset.cpp
@@ -2,6 +2,7 @@
 #include <numeric>
 #include <sstream>
 #include <algorithm>
+#include <cmath>
 
 const int PRIMES[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 
